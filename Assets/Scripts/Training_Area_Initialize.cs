107e546bf146e53ad166fee53a550ca706fddbe5
Akshat Singh <541r0yak54a@gmail.com>
2024-09-03 12:29:40 +0530

    Training area instantiate code fixed

--- Assets/Scripts/Training_Area_Initialize.cs
+++ Assets/Scripts/Training_Area_Initialize.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Training_Area_Initialize : MonoBehaviour
 {
@@ -13,24 +14,19 @@ public class Training_Area_Initialize : MonoBehaviour
 
     private void InitializeTrainingAreas(int numberOfAreas, float areaSize)
     {
-        int right = 0;
-        int left = 1;
-        for (int i = 0; i < numberOfAreas; i++)
-        {
-            Vector3 position = Vector3.zero;
-            if (i % 2 == 0)
-            {
-                // Calculate position for each training area
-                position = new Vector3(-right * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
-                right++;
-            }
-            else
-            {
-                position = new Vector3(left * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
-                left++;
-            }
-            
 
+
+
+        if (numberOfAreas > 0)
+        {
+            int leftRight = (numberOfAreas - 1) / 2;
+            int upDown = (numberOfAreas - 1) - leftRight;
+            int right = 1;
+            int left = 1;
+            int up = 1;
+            int down = 1;
+
+            Vector3 position = Vector3.zero;
             // Instantiate the training area at the calculated position
             GameObject trainingArea = Instantiate(TrainingAreaPrefab, position, Quaternion.identity);
 
@@ -38,7 +34,103 @@ public class Training_Area_Initialize : MonoBehaviour
             trainingArea.transform.SetParent(this.transform);
 
             // Optionally, name the training areas for easy identification
-            trainingArea.name = "TrainingArea_" + i;
+            trainingArea.name = "TrainingArea_" + 0;
+
+
+            for (int i = 0; i < leftRight; i++)
+            {
+                position = Vector3.zero;
+
+                if (i % 2 == 0)
+                {
+                    position = new Vector3(-right * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
+                    right++;
+                }
+                else
+                {
+                    position = new Vector3(left * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
+                    left++;
+                }
+                // Instantiate the training area at the calculated position
+                trainingArea = Instantiate(TrainingAreaPrefab, position, Quaternion.identity);
+
+                // Set the instantiated training area's parent to be this MainGameObject
+                trainingArea.transform.SetParent(this.transform);
+
+                // Optionally, name the training areas for easy identification
+                trainingArea.name = $"TrainingArea_right{right - 1}_left{left - 1}";
+            }
+
+            for (int i = 0; i < upDown; i++)
+            {
+                position = Vector3.zero;
+
+                if (i % 2 == 0)
+                {
+                    position = new Vector3(0, 0, up * areaSize * 2 * 2f); // Arranging areas in a line along the x-axis
+                    up++;
+                }
+                else
+                {
+                    position = new Vector3(0, 0, -down * areaSize * 2 * 2f); // Arranging areas in a line along the x-axis
+                    down++;
+                }
+                // Instantiate the training area at the calculated position
+                trainingArea = Instantiate(TrainingAreaPrefab, position, Quaternion.identity);
+
+                // Set the instantiated training area's parent to be this MainGameObject
+                trainingArea.transform.SetParent(this.transform);
+
+                // Optionally, name the training areas for easy identification
+                trainingArea.name = $"TrainingArea_up{up - 1}_down{down - 1}";
+            }
         }
+        
+
+
+        //// Calculate the midpoint for splitting the areas
+        //int halfNumberOfAreas = numberOfAreas / 2;
+
+        //for (int i = 0; i < numberOfAreas; i++)
+        //{
+        //    Vector3 position = Vector3.zero;
+        //    if (i < halfNumberOfAreas)
+        //    {
+        //        // Arrange the first half of the areas in a left-right line
+        //        if (i % 2 == 0)
+        //        {
+        //            position = new Vector3(-right * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
+        //            right++;
+        //        }
+        //        else
+        //        {
+        //            position = new Vector3(left * areaSize * 2 * 2f, 0, 0); // Arranging areas in a line along the x-axis
+        //            left++;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // Arrange the second half of the areas in an up-down line
+        //        if ((i - halfNumberOfAreas) % 2 == 0)
+        //        {
+        //            position = new Vector3(0, 0, up * areaSize * 2 * 2f); // Arranging areas in a line along the y-axis
+        //            up++;
+        //        }
+        //        else
+        //        {
+        //            position = new Vector3(0, 0, -down * areaSize * 2 * 2f); // Arranging areas in a line along the y-axis
+        //            down++;
+        //        }
+        //    }
+
+        //    // Instantiate the training area at the calculated position
+        //    GameObject trainingArea = Instantiate(TrainingAreaPrefab, position, Quaternion.identity);
+
+        //    // Set the instantiated training area's parent to be this MainGameObject
+        //    trainingArea.transform.SetParent(this.transform);
+
+        //    // Optionally, name the training areas for easy identification
+        //    trainingArea.name = "TrainingArea_" + i;
+        //}
     }
 }
